049b915ce8098a0772469bd5079eedf9a1c31a7a
Zerig <Zerig@users.noreply.github.com>
2020-04-25 16:29:01 +0200

    error

--- src/Console/Log.php
+++ src/Console/Log.php
@@ -11,8 +11,6 @@ class Log{
 		$bt = debug_backtrace();
 		$caller = array_shift($bt);
 
-		echo print_r($caller);
-
 		$GLOBALS["log"]["mysql"][] = new LogData([
 			"sql" 	=> $sql,
 			"file" 	=> $caller['file'],
