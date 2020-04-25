c0dac49425f194a9c30eceefdca054dd7dbf8280
Zerig <Zerig@users.noreply.github.com>
2020-04-25 09:30:01 +0200

    Update Log.php

--- src/Console/Log.php
+++ src/Console/Log.php
@@ -3,6 +3,22 @@ namespace Console;
 
 class Log{
 
+	public static function mysql($sql){
+		$GLOBALS["log"]["mysql"] = $sql;
+ 		return $GLOBALS["log"]["mysql"];
+	}
+
+	public static function getMysql($sql){
+		return $GLOBALS["log"]["mysql"];
+	}
+
+
+
+
+
+
+
+
 
 	public static function include($page_name){
 		$GLOBALS["log"]["include"] = $page_name;
@@ -10,11 +26,8 @@ class Log{
 	}
 
 
-	public static function mysql($sql){
-		$GLOBALS["log"]["mysql"] = $sql;
- 		return $GLOBALS["log"]["mysql"];
-	}
-	
+
+
 
 	public static function module($sql){
 		if(function_exists("log__module_add")) log__module_add($sql);
